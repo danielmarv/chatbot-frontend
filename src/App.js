483ef051d340df014f816dc91426f3c434ecda6a
Daniel Ntege <danientege785@gmail.com>
2023-08-23 09:27:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userMessage, setUserMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [botResponse, setBotResponse] = useState('');
+
+  const handleSendMessage = async () => {
+    if (userMessage.trim() === '') return;
+
+    // Send user message to the backend
+    const response = await fetch('/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ message: userMessage }),
+    });
+
+    const responseData = await response.json();
+
+    // Update chat history
+    setMessages([...messages, { role: 'user', content: userMessage }]);
+    setMessages([...messages, { role: 'bot', content: responseData.botResponse }]);
+
+    setUserMessage('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chat-window">
+        {messages.map((message, index) => (
+          <div key={index} className={`message ${message.role}`}>
+            {message.content}
+          </div>
+        ))}
+      </div>
+      <div className="input-area">
+        <input
+          type="text"
+          value={userMessage}
+          onChange={e => setUserMessage(e.target.value)}
+          placeholder="Type your message..."
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+      <div className="response-area">
+        <p>Bot: {botResponse}</p>
+      </div>
     </div>
   );
 }
